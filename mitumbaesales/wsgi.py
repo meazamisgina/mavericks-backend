771863a1822ef1876a9d88483268e0c703301a21
Meaza Misgina <misginameaza@gmail.com>
2025-12-28 12:07:45 -0800

    migrate to vercel with Neon DB

--- mitumbaesales/wsgi.py
+++ mitumbaesales/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mitumbaesales.settings')
 
 application = get_wsgi_application()
+
+app = application  # This tells Vercel where the entry point is
